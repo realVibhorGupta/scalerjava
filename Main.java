d08e786d090eb4b7129d6987f169fb3d1cf720b5
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2022-09-28 08:50:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Vibhor");
+        System.out.println("Vibhor");
+
+//        variables  and data types
+        char c = 'a';
+        System.out.println(c);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+
+    }
