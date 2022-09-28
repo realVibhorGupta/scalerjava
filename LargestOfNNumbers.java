3d3dd6ea96c285f029bdcba564baa2cbdc7071fd
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2022-09-28 09:14:38 +0000

    Created new Class LargestOfNNumbers

---
+++ LargestOfNNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LargestOfNNumbers {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of Elements");
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int maxValue = Integer.MIN_VALUE;
+
+        for(int i = 1 ; i <=n ; i++){
+            int current = scanner.nextInt();
+            maxValue = Math.max(maxValue,current);
+        }
+        System.out.println(maxValue);
+
+    }
+}
