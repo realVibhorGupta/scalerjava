b27a5d30466d270408ccb16fcc862aafb39c03b7
Vibhor <vibhor.88.gupta@gmail.com>
2022-10-02 00:23:17 +0530

    Created new Class PrintListOfNumberrs

---
+++ PrintListOfNumberrs.java
@@ -0,0 +1,19 @@
+public class PrintListOfNumberrs {
+
+    public static void main(String[] args) {
+
+//        for (int  i =  1 ;  i < 11 ;  i++) {
+//            System.out.println(i);
+//
+//        }
+//    using while loops
+        int i = 1;
+        while(i<11){
+            System.out.println(i);
+            i++;
+
+        }
+    }
+
+
+}
