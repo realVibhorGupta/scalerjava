eaa9c83aca359e74cab906b8e2a6635185d8d153
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2023-09-08 22:06:08 -0700

    Some more Classes

--- recursion2/LinearSearchWithRecursion.java
+++ recursion2/LinearSearchWithRecursion.java
@@ -12,8 +12,78 @@
 
 package recursion2;
 
+import java.util.ArrayList;
+
 public class LinearSearchWithRecursion {
+
+    static boolean linearSearchInArrayWithRecursion(int[] array, int target, int index) {
+        if (array.length == 0) {
+            return false;
+        }
+        if (index > array.length - 1) {
+            return false;
+        }
+        return array[index] == target || linearSearchInArrayWithRecursion(array, target, index + 1);
+    }
+
+    static int findIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return -1;
+        }
+        if (arr[index] == target) {
+            return index;
+        } else {
+            return findIndex(arr, target, index + 1);
+        }
+
+    }
+
+
+    private static int findIndexFromLast(int[] arr, int target, int index) {
+        if (index == -1) {
+            return -1;
+        }
+        if (arr[index] == target) {
+            return index;
+        } else {
+            return findIndexFromLast(arr, target, index - 1);
+        }
+    }
+
+    static ArrayList<Integer> arrayList = new ArrayList<>();
+
+    static void findAllIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target) {
+            arrayList.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+    }
+
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> arrayList) {
+        if (index == arr.length) {
+            return arrayList;
+        }
+        if (arr[index] == target) {
+            arrayList.add(index);
+        }
+        return findAllIndex(arr, target, index + 1, arrayList);
+    }
+
     public static void main(String[] args) {
+
+        int[] arr = {1, 22, 33, 14, 5, 16, 7, 8, 9};
+        int[] array = {1, 2, 3, 4, 5, 5, 5, 5, 16, 7, 8, 9};
+        System.out.println(linearSearchInArrayWithRecursion(arr, 15, 0));
+
+        System.out.println(findIndex(arr, 22, 0));
+        System.out.println(findIndexFromLast(arr, 5, arr.length - 1));
+        findAllIndex(array, 5, 0);
+        System.out.println(arrayList);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(list);
 
     }
 }
---
+++ recursion2/SelectionSort.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023. , Sunshine.Code <sunshinethelearningsite@gmail.com>
+ *   License: MIT
+ * The MIT License (MIT).
+ * Copyright © 2023, Sunshine.Code <sunshinethelearningsite@gmail.com>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy   of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: .The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *  .
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package recursion2;
+
+public class SelectionSort {
+    static void selection(int[] arr, int r, int c, int max) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            if (arr[c] > arr[max]) {
+                selection(arr, r, c+1, c);
+            } else {
+                selection(arr, r, c+1, max);
+            }
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+            selection(arr, r-1, 0, 0);
+        }
+    }
+
+}
