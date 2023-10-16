9929dc0d54929815a080589b50584654418ceab2
unknown <vibhor.88.gupta@gmail.com>
2023-10-16 00:02:13 -0700

    DFS using Stack

---
+++ trees/DFSUsingStacks.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023. , Sunshine.Code <sunshinethelearningsite@gmail.com>
+ *   License: MIT
+ * The MIT License (MIT).
+ * Copyright © 2023, Sunshine.Code <sunshinethelearningsite@gmail.com>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy   of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: .The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *  .
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package trees;
+
+import com.sun.source.tree.Tree;
+
+import java.util.Stack;
+
+public class DFSUsingStacks {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    void dfsStack(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()){
+            TreeNode  removed =  stack.pop();
+            System.out.println(removed.val + " === ");
+            if(removed.right != null ){
+                stack.push(removed.right);
+            }
+            if(removed.left != null ){
+                stack.push(removed.left);
+            }
+        }
+    }
+}
